f8d951cc37eee5515202a898bf64026e29ec66d8
Dekroo <dekroostream@gmail.com>
2025-06-11 18:03:53 +0200

    Font implementation

--- app/src/main/java/com/example/newsreader/ui/theme/Type.kt
+++ app/src/main/java/com/example/newsreader/ui/theme/Type.kt
@@ -1,20 +1,25 @@
 package com.example.newsreader.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.newsreader.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontWeight = FontWeight.Bold,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
+    ),
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
@@ -32,3 +37,14 @@ val Typography = Typography(
     )
     */
 )
+
+val geistFontFamily = FontFamily(
+    Font(R.font.geist_regular, FontWeight.Normal),
+    Font(R.font.geist_medium, FontWeight.Medium),
+    Font(R.font.geist_semibold, FontWeight.SemiBold),
+    Font(R.font.geist_bold, FontWeight.Bold),
+)
+@Composable
+fun getGeistFontFamily(): FontFamily {
+    return if (LocalInspectionMode.current) FontFamily.Default else geistFontFamily
+}

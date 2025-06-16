32c8cbde928b2987128c65a184c6771b4e252ee3
Dekroo <dekroostream@gmail.com>
2025-06-16 15:41:08 +0200

    CategoryNewsPage

--- app/src/main/java/com/example/newsreader/ui/pages/CategoryNews.kt
+++ app/src/main/java/com/example/newsreader/ui/pages/CategoryNews.kt
@@ -1,5 +1,7 @@
 package com.example.newsreader.ui.pages
 
+import android.util.Log
+import android.util.Log.e
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.tooling.preview.Preview
@@ -7,11 +9,14 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.newsreader.ui.ApiViewModel
 import com.example.newsreader.ui.elements.NewsElement
+import androidx.compose.runtime.getValue
 
 @Composable
-fun BitcoinNews(navController: NavController, viewModel: ApiViewModel = viewModel()) {
+fun CategoryNews(navController: NavController, viewModel: ApiViewModel = viewModel()) {
+    val category by viewModel.selectedCategory
     LaunchedEffect(Unit) {
-        viewModel.onBitcoin()
+        viewModel.onCategory(category)
+        Log.e("Selected category", category)
     }
     NewsElement(navController = navController, viewModel = viewModel)
 }
@@ -20,6 +25,6 @@ fun BitcoinNews(navController: NavController, viewModel: ApiViewModel = viewMode
 @Preview
 @Composable
 fun BitcoinNewsPreview() {
-    BitcoinNews(navController = TODO(), viewModel = viewModel())
+    CategoryNews(navController = TODO(), viewModel = viewModel())
     NewsElement( navController = TODO(), viewModel = viewModel())
 }
